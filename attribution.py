7ba929a5683ad03917472c28607ffd115021bd62
mukul <mukul.kr99@gmail.com>
2022-01-16 09:35:50 +0530

    captum vis

--- attribution.py
+++ attribution.py
@@ -15,6 +15,7 @@ class GraphAttributions:
     """
 
     def __init__(self, model_path: Path, config: dict, graph_path: Path, graph_info_path: Path) -> None:
+        self.config = config
         self.driver = GraphInference(model_path, config)
         self.driver.load_dataset(graph_path, graph_info_path)
 
@@ -25,19 +26,19 @@ class GraphAttributions:
         batch_x_emb = batch_x_emb[None]
         score = self.driver.model.infer(batch_x_emb, batch_e, batch_graphs, batch_lap_pos_enc)
         pred_score = float(score[0])
-        baseline_batch_x = torch.zeros_like(batch_x_emb).to(config['device'])
+        baseline_batch_x = torch.zeros_like(batch_x_emb).to(self.config['device'])
         ig = IntegratedGradients(partial(self.driver.model.infer, e=batch_e, g=batch_graphs, h_lap_pos_enc=batch_lap_pos_enc))
         attributions, delta = ig.attribute(batch_x_emb, baseline_batch_x, target=0, return_convergence_delta=True)
         attributions = attributions.sum(dim=2).squeeze(0)
         attributions = attributions / torch.norm(attributions)
         attributions = attributions.cpu().detach().numpy()
 
-        return original_score, pred_score, attributions, texts
+        return original_score, pred_score, attributions, texts, delta
 
     def exec_on_list(self, graph_ids: list, output_dir: Path) -> None:
         results = []
         for graph_id in tqdm(graph_ids, desc="Processing .."):
-            original_score, pred_score, attributions, texts = self.get_attributions(graph_id)
+            original_score, pred_score, attributions, texts, delta = self.get_attributions(graph_id)
             text_weights = [f"{t} ({w : .4f})" for t, w in zip(texts, attributions)]
             results.append((graph_id, str(original_score), str(pred_score),  " ".join(text_weights)))
 
